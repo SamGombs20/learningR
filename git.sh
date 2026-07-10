6c48195eff61c7c81fa0d548dfdc0ddc7a407713
SamGombs20 <gombajosh18@gmail.com>
2026-07-10 16:33:42 +0300

    Update:Learnt the syntax, data types, and worked with some data

---
+++ git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Check if the commit message was supplied
+if [ -z "$1" ]
+then
+	echo "Please provide commit message"
+	exit 1
+fi
+
+#Run git commands
+git add .
+git commit -m "$1"
+git push 
